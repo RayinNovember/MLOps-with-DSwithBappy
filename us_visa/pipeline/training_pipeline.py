0c469b552c38f690d05ec34bb8042c948e6fbc51
Ray <wangchangrui1815@gmail.com>
2026-02-22 20:11:05 -0500

    Add data transformation component

--- us_visa/pipeline/training_pipeline.py
+++ us_visa/pipeline/training_pipeline.py
@@ -3,17 +3,16 @@ from us_visa.exception import USvisaException
 from us_visa.logger import logging
 from us_visa.components.data_ingestion import DataIngestion
 from us_visa.components.data_validation import DataValidation
+from us_visa.components.data_transformation import DataTransformation
 
-from us_visa.entity.config_entity import DataIngestionConfig
-from us_visa.entity.config_entity import DataValidationConfig
-
-from us_visa.entity.artifact_entity import DataIngestionArtifact
-from us_visa.entity.artifact_entity import DataValidationArtifact
+from us_visa.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
+from us_visa.entity.artifact_entity import DataIngestionArtifact,DataValidationArtifact,DataTransformationArtifact
 
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
         self.data_validation_config = DataValidationConfig()
+        self.data_transformation_config = DataTransformationConfig()
         
     def start_data_ingestion(self) -> DataIngestionArtifact:
         """
@@ -37,26 +36,36 @@ class TrainPipeline:
         logging.info("Entered the start_data_validation method of TrainPipeline class")
 
         try:
-            data_validation = DataValidation(data_ingestion_artifact=data_ingestion_artifact,
-                                             data_validation_config=self.data_validation_config)
-
+            data_validation = DataValidation(data_ingestion_artifact=data_ingestion_artifact,data_validation_config=self.data_validation_config)
             data_validation_artifact = data_validation.initiate_data_validation()
 
             logging.info("Performed the data validation operation")
-
-            logging.info(
-                "Exited the start_data_validation method of TrainPipeline class"
-            )
+            logging.info("Exited the start_data_validation method of TrainPipeline class")
 
             return data_validation_artifact
 
         except Exception as e:
             raise USvisaException(e, sys) from e
         
+    def start_data_transformation(self,data_ingestion_artifact:DataIngestionArtifact, data_validation_artifact:DataTransformation) -> None:
+        try:
+            data_transformation = DataTransformation(data_ingestion_artifact=data_ingestion_artifact,
+                                                     data_transformation_config=self.data_transformation_config,
+                                                     data_validation_artifact=data_validation_artifact)
+            data_transformation_artifact = data_transformation.initiate_data_transformation()
+            return data_transformation_artifact
+        except Exception as e:
+            raise USvisaException(e,sys)
+            
+      
                 
     def run_pipeline(self,) -> None:
         try:
             data_ingestion_artifact = self.start_data_ingestion()
             data_validation_artifact = self.start_data_validation(data_ingestion_artifact = data_ingestion_artifact)
+            data_transformation_artifact = self.start_data_transformation(data_ingestion_artifact = data_ingestion_artifact,
+                                                                          data_validation_artifact = data_validation_artifact)
         except Exception as e:
             raise USvisaException(e,sys) from e
+        
+        
