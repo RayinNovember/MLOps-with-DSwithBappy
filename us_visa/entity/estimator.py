9e4958dde5e3c16c3f6c982a0828bbd67c1da79f
Ray <wangchangrui1815@gmail.com>
2026-02-22 21:39:01 -0500

    Add model trainer component

--- us_visa/entity/estimator.py
+++ us_visa/entity/estimator.py
@@ -16,3 +16,30 @@ class TargetValueMapping:
     def reverse_mappint(self):
         mapping_response = self._asdict
         return dict(zip(mapping_response.values(),mapping_response.keys()))
+
+class USvisaModel:
+    def __init__(self, preprocessing_object:Pipeline, trained_model_object:object):
+        self.preprocessing_object = preprocessing_object
+        self.trained_model_object = trained_model_object
+    
+    def predict(self,dataframe: DataFrame) -> DataFrame:
+        """
+        Function accepts raw inputs and then transforms raw input using preprocessing_object which guarantees that the inputs are in the same format as the training data.
+        At last it performs prediction on transformed features.
+        """
+        logging.info("Entered predict method")
+        try:
+            logging.info("using the trained model to get prediction")
+            transformed_feature = self.preprocessing_object.transform(dataframe)
+            logging.info("used the trained model to get predictions")
+            return self.trained_model_object.predict(transformed_feature)
+        except Exception as e:
+            raise USvisaException(e,sys) from e
+               
+    def __repr__(self):
+        return f"{type(self.trained_model_object).__name__}()"
+    
+    def __str__(self):
+        return f"{type(self.trained_model_object).__name__}()"   
+        
+    
